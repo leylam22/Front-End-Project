787ec93d5dc692d4caa4990320849e303e0358d8
leylam22 <leylarm@code.edu.az>
2023-05-28 03:11:28 +0400

    added some changes

--- assets/indexx/index.js
+++ assets/indexx/index.js
@@ -10,8 +10,28 @@ const selectBtn2 = optionMenu2.querySelector('.dropbtn2')
 selectBtn2.addEventListener('click', ()=> optionMenu2.classList.toggle('active'))
 //First top bar Dropdown End
 
-// const optionMenuu=document.querySelector('.select-menuu'),
-//         selectBtnn =optionMenuu.querySelector('.select-btnn'),
-//         options=optionMenuu.querySelector('.optionn')
+// Count Down Start
+var countDownDate = new Date("May 30, 2023 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
 
-//         selectBtnn.addEventListener('click', ()=> optionMenuu.classList.toggle('active'))
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector('#days').innerHTML = '00';
+        document.querySelector('#hours').innerHTML = '00';
+        document.querySelector('#minutes').innerHTML = '00';
+        document.querySelector('#seconds').innerHTML = '00';
+    }
+}, 1000);
+
