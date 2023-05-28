5563574215b193ac76bd04f1272f756a1bc34f50
leylam22 <leylarm@code.edu.az>
2023-05-29 03:00:13 +0400

    blog page is done

---
+++ assets/blog/blog.js
@@ -0,0 +1,26 @@
+// Render Products Start
+const container=document.querySelector('.card-container')
+function createProduct(data) {
+    const card = document.createElement('div')
+    
+    card.innerHTML=`<div class="card">
+    <img src="./assets/images/${data.img}" alt="">
+    <h4>${data.title}</h4>
+    <div>
+        <a href="#">FASHION AND STYLE</a>
+        <span>${data.date}</span>
+    </div>
+</div>`
+
+    return card;
+}
+function renderProducts() {
+    container.innerHTML = "";
+    let _temp = [...data];
+    _temp.forEach((x) => {
+      const productDiv = createProduct(x);
+      container.append(productDiv);
+    });
+}
+renderProducts()
+//Render Products End
---
+++ assets/const/dataForBlog.js
@@ -0,0 +1,56 @@
+const data= [
+    {
+        id:1,
+        img:"post1.jpg",
+        title:'Pellentesque habitant morbi',
+        date:'January 16, 2019'
+    },
+    {
+        id:2,
+        img:"post2.jpg",
+        title:'Best books about Fashion',
+        date:'January 16, 2019'
+    },
+    {
+        id:3,
+        img:"post3.jpg",
+        title:'Best books about Fashion',
+        date:'January 16, 2019'
+    },
+    {
+        id:4,
+        img:"post4.jpg",
+        title:'Autumn fashion tips and tricks',
+        date:'January 16, 2019'
+    },
+    {
+        id:5,
+        img:"post5.jpg",
+        title:'Newest photo apps',
+        date:'January 16, 2019'
+    },
+    {
+        id:6,
+        img:"post6.jpg",
+        title:'Best books about Photography',
+        date:'January 16, 2019'
+    },
+    {
+        id:7,
+        img:"post2.jpg",
+        title:'Best books about Fashion',
+        date:'January 16, 2019'
+    },
+    {
+        id:8,
+        img:"post1.jpg",
+        title:'Pellentesque habitant morbi',
+        date:'January 16, 2019'
+    },
+    {
+        id:9,
+        img:"post3.jpg",
+        title:'Best books about Fashion',
+        date:'January 16, 2019'
+    }
+]
