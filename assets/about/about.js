8aa3f185660d0702cbbb31bc6426c55845ebd84d
leylam22 <leylarm@code.edu.az>
2023-05-29 21:14:06 +0400

    About page is completed

---
+++ assets/about/about.js
@@ -0,0 +1,32 @@
+// Render Products Start
+const container=document.querySelector('.card-container')
+function createProduct(data) {
+    const card = document.createElement('div')
+    
+    card.innerHTML=`<div class="card">
+    <img src="./assets/images/${data.img}" alt="">
+    <h5>${data.name}</h5>
+    <p>${data.position}</p>
+</div>`
+
+    return card;
+}
+function renderProducts() {
+    container.innerHTML = "";
+    let _temp = [...data];
+    _temp.forEach((x) => {
+      const productDiv = createProduct(x);
+      container.append(productDiv);
+    });
+}
+renderProducts()
+//Render Products End
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
---
+++ assets/const/peopleData.js
@@ -0,0 +1,50 @@
+const data=[
+    {
+        id:1,
+        img:'avatar-1.jpg',
+        name:'Frankie Kafka',
+        position:'CEO & Founder'
+    },
+    {
+        id:2,
+        img:'avatar-2.jpg',
+        name:'Brittney Day',
+        position:'CTO'
+    },
+    {
+        id:3,
+        img:'avatar-3.jpg',
+        name:'Kim Cervantes',
+        position:'Marketing'
+    },
+    {
+        id:4,
+        img:'avatar-4.jpg',
+        name:'Massey Armstrong',
+        position:'Customer care'
+    },
+    {
+        id:5,
+        img:'avatar-5.jpg',
+        name:'B.J. Petersen',
+        position:'E-commerce'
+    },
+    {
+        id:6,
+        img:'avatar-6.jpg',
+        name:'Mayra Holman',
+        position:'Customer care'
+    },
+    {
+        id:7,
+        img:'avatar-7.jpg',
+        name:'Gabriel Fisher',
+        position:'Customer care'
+    },
+    {
+        id:8,
+        img:'avatar-8.jpg',
+        name:'Jose Holman',
+        position:'Customer care'
+    },
+]
