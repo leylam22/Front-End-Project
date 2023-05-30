96bf31ce4e58fade4c867dcd90e53d50579eed41
leylam22 <leylarm@code.edu.az>
2023-05-31 02:40:13 +0400

    carusel added

--- assets/indexx/index.js
+++ assets/indexx/index.js
@@ -66,3 +66,16 @@ function renderProducts() {
 }
 renderProducts()
 //Render Products End
+
+ var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
