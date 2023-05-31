fe9d4fd1039f60917bef1c362bda17dbc6e037dd
leylam22 <leylarm@code.edu.az>
2023-05-31 04:19:00 +0400

    blog paralax added

--- assets/blog/blog.js
+++ assets/blog/blog.js
@@ -32,3 +32,11 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  const img = document.querySelector('.section1').querySelector('.img')
+new simpleParallax(img, {
+	delay: 1.6,
+    scale: 1.5,
+    overflow:true,
+	transition: 'cubic-bezier(0,0,0,1)'
+});
